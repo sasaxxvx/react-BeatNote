d093e689ba123c98522a83572660088a6f2f9064
sasaxxvx <weizuo0902@gmail.com>
2020-08-09 22:49:10 +1000

    second commit

---
+++ build/precache-manifest.dd814e84e04a0eb98fcf78b30ddb6963.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b802ef7707125c94f5a6e67e5b92eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa09b42d5f11877488c",
+    "url": "/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "fe319ee69780a206b60d",
+    "url": "/static/css/main.8c7acd15.chunk.css"
+  },
+  {
+    "revision": "faa09b42d5f11877488c",
+    "url": "/static/js/2.319ee88a.chunk.js"
+  },
+  {
+    "revision": "c741a9512219825c71dd1ad1178add62",
+    "url": "/static/js/2.319ee88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe319ee69780a206b60d",
+    "url": "/static/js/main.b06ebef3.chunk.js"
+  },
+  {
+    "revision": "505e4dc665c09938bf7e",
+    "url": "/static/js/runtime-main.9075e973.js"
+  },
+  {
+    "revision": "e9b68ba107a4b5b4ce4249c258e53d20",
+    "url": "/static/media/austin-edwards-QcO0TNNoqig-unsplash.e9b68ba1.jpg"
+  }
+]);
